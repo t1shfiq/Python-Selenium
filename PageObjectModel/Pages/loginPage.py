76b415ce8b0270b9d94de860b7c479b94c857c5c
t1shfiq <alitashfiq@Alis-MacBook-Pro.local>
2020-10-07 02:44:01 -0400

    Implementation of Page Object Model
    Made two different classes for homePage and loginPage.

---
+++ PageObjectModel/Pages/loginPage.py
@@ -0,0 +1,22 @@
+class LoginPage():
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        #objects
+        self.username_testbox_id = "txtUsername"
+        self.password_textbox_id = "txtPassword"
+        self.login_button_id = "btnLogin"
+
+    #all actions to be performed on the objects
+    
+    def enter_username(self, username):
+        self.driver.find_element_by_id(self.username_textbox_id).clear()
+        self.driver.find_element_by_id(self.username_textbox_id).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element_by_id(self.password_textbox_id).clear()
+        self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element_by_id(self.login_button_id).click(0)
--- PageObjectModel/Tests/login.py
+++ PageObjectModel/Tests/login.py
@@ -3,6 +3,9 @@ import time
 #Python's in-buiilt testing module
 import unittest
 
+from python_selenium.PageObjectModel.Pages.loginPage import LoginPage
+from python_selenium.PageObjectModel.Pages.loginPage import HomePage
+
 class LoginTest(unittest.TestCase):
 
     ## annotation for using class functions
@@ -15,12 +18,25 @@ class LoginTest(unittest.TestCase):
 
     ## test for a valid log in
     def test_login_valid(self):
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
-        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
-        self.driver.find_element_by_id("btnLogin").click()
-        self.driver.find_element_by_id("welcome").click()
-        self.driver.find_element_by_link_text("Logout").click()
+        driver = self.driver
+
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = LoginPage(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = HomePage(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+        
+        # self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # self.driver.find_element_by_id("btnLogin").click()
+        #self.driver.find_element_by_id("welcome").click()
+        #self.driver.find_element_by_link_text("Logout").click()
+        
         time.sleep(5)
 
     @classmethod
